f2c99817d7e2737ce38b44b82475acb063507ccf
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-08-09 19:18:58 -0300

    adicionado botao

--- scripts.js
+++ scripts.js
@@ -12,3 +12,13 @@ const myObserver = new IntersectionObserver( (entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach( (element) => myObserver.observe(element))
+
+const botao = document.querySelector(".btn_top")
+
+window.addEventListener("scroll", function(event) {
+    if (this.window.scrollY == 0) {
+        botao.classList.remove("visible")
+    } else {
+        botao.classList.add("visible")
+    }
+})
